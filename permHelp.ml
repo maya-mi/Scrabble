e567ca16e313ad710e9630a7a232658467b1263b
Charlie O'Mara <comara@college.harvard.edu>
2017-05-03 15:27:58 -0400

    adf'

--- permHelp.ml
+++ permHelp.ml
@@ -1,15 +1,16 @@
+
+
+(* Permutation generation code from More Ocaml by John Witington *)
+
 let rec interleave e seen l =
   match l with
   | [] -> [seen @ [e]]
   | x::xs -> (seen @ e :: x :: xs) :: interleave e (seen @ [x]) xs
 ;;
 
-
-
 let combine x ps =
   List.concat (List.map (interleave x []) ps)
 ;;
-
 
 let rec perm1 p =
   match p with
@@ -17,8 +18,3 @@ let rec perm1 p =
   | h::t -> combine h (perm1 t)
 
 ;;
-
-
-
-
-(*print_int (List.length (perm1 [0; 1; 2; 3; 4; 5; 6; 7; 8])) ;;*)
